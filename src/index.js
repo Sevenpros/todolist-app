dd85f1e698e6e27369e80c0c75176da3cfcf443f
Sevenpros <sethbisoso@gmail.com>
2022-08-10 15:11:47 +0200

    Add functionalities for todo app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TodoContainer from './functionBased/components/TodoContainer';
+import "./functionBased/App.css";
+import { BrowserRouter as Router } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import About from "./functionBased/pages/About"
+import NotMatch from "./functionBased/pages/NotMatch"
+import Navbar from "./functionBased/components/NavBar"
+import "./functionBased/App.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+       <Router>
+          <Navbar />
+          <Routes >
+          <Route path="/" element={<TodoContainer />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="*" element={<NotMatch />}/>
+          </Routes>
+        </Router>
+    </React.StrictMode>
+)
